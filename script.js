40e2143253bafca2804e09c63b1b54fdca815be4
Luke <lrsorrenti@hotmail.com>
2023-10-22 13:44:06 -0500

    added fix to the add fourth and fifth disks in the game, now pressing the fifth disk before the fourth will correctly show all five

--- script.js
+++ script.js
@@ -44,29 +44,48 @@ function isValidMove(tower, disk) {
 
 
 function addDisk(diskNumber) {
-    if (document.querySelector(`#disk${diskNumber}`)) {
-        // The disk already exists; don't add again
-        return;
+    for (let i = 1; i <= diskNumber; i++) {
+        if (!document.querySelector(`#disk${i}`)) {
+            createDisk(i);
+        }
     }
+}
 
+function createDisk(diskNumber) {
     const disk = document.createElement("div");
     disk.classList.add("disk");
     disk.setAttribute("draggable", "true");
     disk.id = `disk${diskNumber}`;
 
-    switch(diskNumber) {
-        case 4:
+    switch (diskNumber) {
+        case 1:
+            // Set properties for disk1 if any, e.g.
+            disk.style.width = "90px";
+            disk.style.backgroundColor = "red";
+            break;
+        case 2:
+            // Set properties for disk2 if any
+            disk.style.width = "120px";
+            disk.style.backgroundColor = "green";
+            break;
+        case 3:
+            // Set properties for disk3 if any
             disk.style.width = "150px";
+            disk.style.backgroundColor = "blue";
+            break;
+        case 4:
+            disk.style.width = "180px";
             disk.style.backgroundColor = "purple";
             break;
         case 5:
-            disk.style.width = "180px";
+            disk.style.width = "210px";
             disk.style.backgroundColor = "yellow";
             break;
     }
 
     document.getElementById("tower1").insertBefore(disk, document.getElementById("tower1").firstChild);
 }
+
 
 function resetGame() {
     const towers = document.querySelectorAll(".tower");
@@ -76,9 +95,23 @@ function resetGame() {
         }
     });
 
-    for(let i = 1; i <= 5; i++) {
-        if (document.querySelector(`#disk${i}`)) {
-            document.getElementById("tower1").appendChild(document.querySelector(`#disk${i}`));
-        }
-    }
+    // Re-add the default disks to tower1
+    const tower1 = document.getElementById("tower1");
+    const disk3 = document.createElement("div");
+    disk3.classList.add("disk");
+    disk3.setAttribute("draggable", "true");
+    disk3.id = "disk3";
+    tower1.appendChild(disk3);
+
+    const disk2 = document.createElement("div");
+    disk2.classList.add("disk");
+    disk2.setAttribute("draggable", "true");
+    disk2.id = "disk2";
+    tower1.appendChild(disk2);
+
+    const disk1 = document.createElement("div");
+    disk1.classList.add("disk");
+    disk1.setAttribute("draggable", "true");
+    disk1.id = "disk1";
+    tower1.appendChild(disk1);
 }
